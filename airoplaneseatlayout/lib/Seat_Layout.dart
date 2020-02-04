22298d91c81c40f244cea6c1b1967eac95456939
Pramod Singh <psing@megasysindia.com>
2020-02-04 14:00:25 +0530

    screen updated

--- airoplaneseatlayout/lib/Seat_Layout.dart
+++ airoplaneseatlayout/lib/Seat_Layout.dart
@@ -1,3 +1,4 @@
+import 'package:airoplaneseatlayout/seatlayout_modal.dart';
 import 'package:flutter/material.dart';
 
 class SeatLayoutClass extends StatefulWidget {
@@ -16,6 +17,7 @@ class _SeatLayoutClassState extends State<SeatLayoutClass> {
         body: Container(
           child: Card(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Row(
                   children: <Widget>[
@@ -26,24 +28,124 @@ class _SeatLayoutClassState extends State<SeatLayoutClass> {
                           "Unqualified",
                           style: TextStyle(fontSize: 25),
                         ),
-                        Icon(Icons.notifications,color: Colors.red,),
+                        Icon(
+                          Icons.notifications,
+                          color: Colors.red,
+                        ),
                       ],
                     )),
                     Expanded(
                         child: Padding(
-                          padding: const EdgeInsets.only(top:8.0,right: 0,left: 8),
-                          child: Text(
-                      "Your Class is not valid for the Service",
-                      style: TextStyle(fontSize: 10),
-                    ),
-                        ))
+                      padding:
+                          const EdgeInsets.only(top: 8.0, right: 0, left: 8),
+                      child: Text(
+                        "Your Class is not valid for the Service",
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    ))
                   ],
                 ),
                 Divider(
                   height: 5,
                   thickness: 0.5,
                   color: Colors.grey,
-                )
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Admin"),
+                          Text("British Airways")
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Boarding Class"),
+                          Text("Economic Class")
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[Text("Passengers"), Text("1")],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[Text("Source"), Text("LHR")],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[Text("Destination"), Text("JFK")],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Purchased Summary"),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Premium Security Check-in"),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Image(
+                        image: AssetImage("images/british-pound.png"),
+                        height: 18,
+                        width: 18,
+                      ),
+                    ),
+                    Text("5")
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Total"),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 9),
+                        child: Image(
+                          image: AssetImage("images/british-pound.png"),
+                          height: 18,
+                          width: 18,
+                        ),
+                      ),
+                      Text("5"),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Would you like to add product(s) from below?"),
               ],
             ),
           ),
@@ -52,3 +154,18 @@ class _SeatLayoutClassState extends State<SeatLayoutClass> {
     );
   }
 }
+
+List<SeatLayoutModal> list = <SeatLayoutModal>[
+  const SeatLayoutModal(
+      title: "No1 Lounge", description: "Express - 1.5 Hours", value: "10"),
+  const SeatLayoutModal(
+      title: "No1 Loungs", description: "3 Hours", value: "15"),
+  const SeatLayoutModal(
+      title: "My Lounge", description: "Express 1.5 Hours", value: "5"),
+  const SeatLayoutModal(
+      title: "My Lounge", description: "3 Hours", value: "12"),
+  const SeatLayoutModal(
+      title: "Chubroerre", description: "No Champaigne", value: "5"),
+  const SeatLayoutModal(
+      title: "No1 Loungs", description: "Discount 3 Hours", value: "5"),
+];
---
+++ airoplaneseatlayout/lib/seatlayout_modal.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class SeatLayoutModal {
+  const SeatLayoutModal({this.title, this.description,this.value});
+
+  final String title;
+  final String description;
+  final String value;
+}
